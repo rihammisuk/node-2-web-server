e91fbbf4353d8da4e7800a7ebb6bf1fa95d27279
rihammisuk <riham659misuk@gmail.com>
2019-02-09 23:31:45 +0600

    Add project page

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.get('/about', (req, res)=>{
     });
 });
 
+app.get('/projects' , (req, res) =>{
+    res.render('projects.hbs', {
+        pageTitle:'Projects',
+        message :'Portfolio page here.'
+    });
+});
 
 app.get('/bad', (req, res) =>{
     res.send({
