b7a3370d0e17ae448ecb1be055ca1a2c5db5fda5
rihammisuk <riham659misuk@gmail.com>
2019-02-09 23:13:59 +0600

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express =  require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
 var app  = express();
 
+//For Partial view Register
 hbs.registerPartials(__dirname + '/views/partials');
+//For hbs view show
 app.set('view engine', 'hbs');
 
+//Middleware 
 app.use((req, res, next) =>{
     var now = new Date().toString();
     var log = `${now} : ${req.method} ${req.url}`;
     console.log(log);
     
-
+    
     fs.appendFile('server.log', log +'\n', (err) =>{
         console.log(err);
         
@@ -23,19 +29,25 @@ app.use((req, res, next) =>{
 //     res.render('maintenance.hbs');
 // });
 
+//For file type extension support (.html, .png, .jpg etc )
 app.use(express.static(__dirname + '/public'));
 
 
 //-----------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------
 
+//Helper method
 hbs.registerHelper('getCurrentYear', () =>{
     return new Date().getFullYear();
 });
 
+//Helper method
 hbs.registerHelper('screamIt', (text)=>{
     return text.toUpperCase();
 });
+
+
+
 app.get(['/', '/home'], (req, res) =>{
     res.render('home.hbs', {
         pageTitle: 'Home Page',
@@ -57,7 +69,9 @@ app.get('/bad', (req, res) =>{
     });
 });
 
-app.listen(3000, () =>{
-    console.log('Server is on post 3000');
+
+//For listen port
+app.listen(port, () =>{
+    console.log(`Server is on post ${port}`);
     
 });
